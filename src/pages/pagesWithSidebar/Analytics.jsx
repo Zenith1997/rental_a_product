df4f0836337da9a59872e37c00b110d442b039fe
Zenith1997 <zenithanthony.official@gmail.com>
2025-06-07 09:48:24 +0530

    bug fix

--- src/pages/pagesWithSidebar/Analytics.jsx
+++ src/pages/pagesWithSidebar/Analytics.jsx
@@ -1,11 +1,13 @@
+import SearchBar from '../../components/Header/SearchBar';
 import Layout from '../../components/Layout';
 import AnalyticsPage from '../../pages/AnalyticsPage/AnalyticsPage';
 
 const AnalyticsPageM = () => {
   return (
-    <>    <div className='bg-white mt-5 flex justify-center shadow-[0_2px_4px_rgba(0,0,0,0.1)]'>
+    <> 
+       <div className='bg-white mt-5 flex justify-center shadow-[0_2px_4px_rgba(0,0,0,0.1)]'>
     <SearchBar/>
-  </div><Layout>
+  </div>
     <Layout>
       <AnalyticsPage />
     </Layout>
